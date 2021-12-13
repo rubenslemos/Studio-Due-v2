771c9986acf878a3d29ab3d0a336825e2b885881
Rubens <rubenslemos@gmail.com>
2021-12-13 07:59:06 -0300

    13/12/21

--- appp/src/store/modules/salao/sagas.js
+++ appp/src/store/modules/salao/sagas.js
@@ -1,8 +1,10 @@
- import {takeLatest, all, call, put} from 'redux-saga/effects'
+ import {takeLatest, all, call, put, select} from 'redux-saga/effects'
  import api from '../../../services/api'
+ import moment from 'moment'
  import consts from '../../../consts'
- import {updateSalao} from './actions'
+ import {updateSalao, updateServicos, updateAgenda, updateColaboradores, updateAgendamento} from './actions'
  import types from './types'
+ import util from '../../../util'
  export function* getSalao(){ 
    try{
     let res = {}
@@ -12,11 +14,63 @@
       alert(res.message)
       return false
     }
-    console.log("res", res)
-    yield put(updateSalao({salao: res.salao}))
+
+    yield put(updateSalao(res.salao))
   } 
   catch(err){
     alert(err.message)
   }
  }
- export default all ([takeLatest(types.GET_SALAO, getSalao)])
+
+ export function* allServicos(){ 
+   try{
+    let res = {}
+    const response = yield call(api.get,`/servico/salao/${consts.salaoId}` )
+    res = response.data
+    if (res.error){
+      alert(res.message)
+      return false
+    }
+
+    yield put(updateServicos(res.servicos))
+  } 
+  catch(err){
+    alert(err.message)
+  }
+ }
+ export function* filterAgenda(){ 
+   try{
+    const {agendamento, agenda} =yield select((state) => state.salao)
+    
+    const finalStartDate = 
+      agenda.length === 0 
+        ? moment().format('YYYY-MM-DD')
+        : Object.keys(agenda[0])[0]
+    
+        console.tron.log('FinalStartDate: ',finalStartDate)
+        console.tron.log('agenda: ',agenda)
+    const {data: res} = yield call(api.post,'/agendamento/dias-disponiveis/',{
+      ...agendamento,
+      data: finalStartDate
+    })
+    if (res.error){
+      alert(res.message)
+      return false
+    }
+    const { horariosDisponiveis, data, colaboradorId } = yield call(util.selectAgendamento, res.agenda)
+    yield put(updateAgenda(res.agenda))
+    const finalDate = moment(`${data}T${horariosDisponiveis[0][0]}`).format()
+    yield put(updateColaboradores(res.colaboradores))
+    yield put(updateAgendamento('data', finalDate))
+    yield put(updateAgendamento('colaboradorId', colaboradorId))
+  } 
+  catch(err){
+    console.log(err.message)
+  }
+ }
+ export default all ([
+  takeLatest(types.GET_SALAO, getSalao),
+  takeLatest(types.ALL_SERVICOS, allServicos),
+  takeLatest(types.FILTER_AGENDA, filterAgenda)
+
+])
